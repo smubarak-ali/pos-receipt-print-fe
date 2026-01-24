9eb340f98d02516b0b25b59d5722b8f2823d823d
Mubarak <mubarakali.00@gmail.com>
2026-01-25 01:20:48 +0800

    Updated the api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://[2001:f40:95a:162c:b622:5f85:7f13:f3b2]:8084/api',
-    discount: 10,
-    webUrl: 'http://localhost:4200',
+    baseUrl: 'https://print-api.abdalipaas.xyz/api',
+    discount: 10
 };
